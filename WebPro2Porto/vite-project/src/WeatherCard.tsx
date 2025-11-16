13fc8b40d5d59556cdfd406d85e18b1728267516
Emil <emilh23@student.sdu.dk>
2025-11-16 15:09:36 +0100

    weather icons setup done

--- WebPro2Porto/vite-project/src/WeatherCard.tsx
+++ WebPro2Porto/vite-project/src/WeatherCard.tsx
@@ -1,29 +1,43 @@
-interface WeatherData {
+import React from "react";
+
+export interface WeatherData {
   typeOfWeather: string;
   timestamp: string;
   temperature: number;
 }
 
 interface WeatherCardProps {
-  data: WeatherData | null;
+  data: WeatherData;
 }
 
+const weatherIcons: Record <string, string> = {
+
+    Sunny: "Icons/sun.png",
+    Rainy: "Icons/rain.png",
+    Snowy: "Icons/snow.png",
+    Thunder: "Icons/thunder.png",
+    Cloudy: "Icons/clouds.png"
+};
+
+const defaultIcon = "Icons/sharknado"
+
 function WeatherCard({ data }: WeatherCardProps) {
-  if (!data) return null;
+     const icon = weatherIcons[data.typeOfWeather] || defaultIcon;
 
   return (
     <div className="card">
-      <img
-        src="Icons/sun.png"
+       <img
+        src={icon}
         className="card-img-top"
         style={{ width: "100px", height: "100px" }}
+        alt={data.typeOfWeather}
       />
 
       <div className="card-body">
         <h5 className="card-title">{data.typeOfWeather}</h5>
         <p className="card-text">Timestamp: {data.timestamp}</p>
         <p className="card-text">Temperature: {data.temperature}°C</p>
-        <a href="#" className="btn btn-primary">View Weather History</a>
+        <button className="btn btn-primary">View Weather History</button>
       </div>
     </div>
   );
